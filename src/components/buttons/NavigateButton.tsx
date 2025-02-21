07361c1dd2db0ab5f51c36b2c2ca4cf1068018ba
mrboom192 <mrboom192@gmail.com>
2025-02-21 01:14:06 -0600

    updated navigation button to use touchable opacity

--- src/components/buttons/NavigateButton.tsx
+++ src/components/buttons/NavigateButton.tsx
@@ -1,4 +1,4 @@
-import { Text, Pressable, View } from "react-native";
+import { Text, Pressable, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
@@ -8,7 +8,6 @@ interface NavigateButtonProps {
   leftIcon?: React.ReactNode; // Optional icon to display in the button
   handleSubmit?: () => void;
   theme?: "dark" | "light" | "transparent"; // Theme for the button
-  border?: boolean;
   href: any;
 }
 
@@ -18,7 +17,6 @@ const NavigateButton: React.FC<NavigateButtonProps> = ({
   rightIcon,
   handleSubmit,
   theme = "dark", // Default theme is dark
-  border = true,
   href,
 }) => {
   // Dynamic styles based on the theme
@@ -26,41 +24,28 @@ const NavigateButton: React.FC<NavigateButtonProps> = ({
   const isLightTheme = theme === "light";
 
   return (
-    <View
-      className={`rounded-lg overflow-hidden ${
-        border
-          ? isLightTheme
-            ? "border border-[#ccc]"
-            : "border border-[#2C2C2C]"
-          : ""
-      } ${isDarkTheme ? "bg-[#2C2C2C]" : isLightTheme ? "bg-white" : ""}`}
-    >
-      <Link href={href} asChild>
-        <Pressable
-          android_ripple={{
-            color: isDarkTheme ? "#444" : isLightTheme ? "#ddd" : "#ccc", // Ripple color based on theme
-            borderless: false,
-          }}
-          className="py-2.5 px-6 justify-center flex-row items-center gap-2"
-          accessibilityLabel={title}
-          onPress={handleSubmit}
+    <Link href={href} asChild>
+      <TouchableOpacity
+        className={`rounded-lg overflow-hidden py-2.5 px-6 justify-center flex-row items-center gap-2 
+  ${isDarkTheme ? "bg-[#2C2C2C]" : isLightTheme ? "bg-white" : ""}`}
+        accessibilityLabel={title}
+        onPress={handleSubmit}
+      >
+        {leftIcon}
+        <Text
+          className={`uppercase ${
+            isDarkTheme
+              ? "text-white"
+              : isLightTheme
+              ? "text-black"
+              : "text-gray-800"
+          }`}
         >
-          {leftIcon}
-          <Text
-            className={`uppercase ${
-              isDarkTheme
-                ? "text-white"
-                : isLightTheme
-                ? "text-black"
-                : "text-gray-800"
-            }`}
-          >
-            {title}
-          </Text>
-          {rightIcon}
-        </Pressable>
-      </Link>
-    </View>
+          {title}
+        </Text>
+        {rightIcon}
+      </TouchableOpacity>
+    </Link>
   );
 };
 

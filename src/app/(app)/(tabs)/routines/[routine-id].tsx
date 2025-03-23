e74761a1bbc818fdcc7a2ef59d715648efde1155
mrboom192 <mrboom192@gmail.com>
2025-03-23 18:57:44 -0500

    Add image picker package

---
+++ src/app/(app)/(tabs)/routines/[routine-id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RoutinePage = () => {
+  return (
+    <View>
+      <Text>Routine Page</Text>
+    </View>
+  );
+};
+
+export default RoutinePage;

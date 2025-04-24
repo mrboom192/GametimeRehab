bba26024cf8edcc5f2742a78c526f9687a79d83d
mrboom192 <mrboom192@gmail.com>
2025-04-23 22:11:09 -0500

    Removed unused stuff

--- src/components/ExerciseHeader.tsx
+++ src/components/ExerciseHeader.tsx
@@ -11,17 +11,11 @@ import Colors from "../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Link, router, usePathname, useSegments } from "expo-router";
 import { useCart } from "../contexts/CartContext";
-import { useSearch } from "../contexts/SearchContext";
 import { TextSemiBold } from "./StyledText";
 
 const ExerciseHeader = () => {
   const segments = useSegments();
   const { cart } = useCart(); // Access the cart state
-  const { searchQuery, setSearchQuery } = useSearch();
-
-  const updateSearch = (query: string) => {
-    setSearchQuery(query);
-  };
 
   return (
     <SafeAreaView style={{ backgroundColor: "#FFF" }}>
@@ -30,11 +24,10 @@ const ExerciseHeader = () => {
           paddingHorizontal: 16,
           paddingBottom: 16,
           flexDirection: "row",
-          gap: 16,
+          justifyContent: "space-between",
         }}
       >
-        {/* Back Button */}
-        {segments[segments.length - 1] !== "routines" && (
+        {segments[segments.length - 1] !== "routines" ? (
           <TouchableOpacity
             style={{
               padding: 16,
@@ -52,44 +45,24 @@ const ExerciseHeader = () => {
           >
             <Ionicons name="chevron-back" color={Colors.dark} size={20} />
           </TouchableOpacity>
+        ) : (
+          <View />
         )}
 
-        {/* Search Button */}
-        <Link href={"/search"} asChild>
-          <TouchableOpacity
-            style={{
-              borderRadius: 9999,
-              flex: 1,
-              backgroundColor: "#FFF",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              shadowColor: "#000",
-              shadowOffset: { width: 0, height: 4 },
-              shadowOpacity: 0.2,
-              shadowRadius: 4,
-              elevation: 5,
-            }}
-          >
-            <TextSemiBold
-              style={{
-                padding: 16,
-                fontFamily: "dm-sb",
-              }}
-            >
-              Search Exercises
-            </TextSemiBold>
-          </TouchableOpacity>
-        </Link>
-
-        {/* Cart Button */}
-        {cart.exercises.length > 0 && (
-          <Link href="/(app)/(modals)/cart" asChild>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 16,
+          }}
+        >
+          {/* Search Button */}
+          <Link href={"/search"} asChild>
             <TouchableOpacity
               style={{
                 padding: 16,
                 borderRadius: 9999,
-                backgroundColor: Colors.green,
+                backgroundColor: "#FFF",
+                flexDirection: "row",
                 justifyContent: "center",
                 alignItems: "center",
                 shadowColor: "#000",
@@ -99,20 +72,42 @@ const ExerciseHeader = () => {
                 elevation: 5,
               }}
             >
-              <Text
-                style={{
-                  height: 20,
-                  width: 20,
-                  textAlign: "center",
-                  fontFamily: "dm-sb",
-                  color: "#FFF",
-                }}
-              >
-                {cart.exercises.length}
-              </Text>
+              <Ionicons name="search-outline" size={20} color={Colors.dark} />
             </TouchableOpacity>
           </Link>
-        )}
+
+          {/* Cart Button */}
+          {cart.exercises.length > 0 && (
+            <Link href="/(app)/(modals)/cart" asChild>
+              <TouchableOpacity
+                style={{
+                  padding: 16,
+                  borderRadius: 9999,
+                  backgroundColor: Colors.green,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  shadowColor: "#000",
+                  shadowOffset: { width: 0, height: 4 },
+                  shadowOpacity: 0.2,
+                  shadowRadius: 4,
+                  elevation: 5,
+                }}
+              >
+                <Text
+                  style={{
+                    height: 20,
+                    width: 20,
+                    textAlign: "center",
+                    fontFamily: "dm-sb",
+                    color: "#FFF",
+                  }}
+                >
+                  {cart.exercises.length}
+                </Text>
+              </TouchableOpacity>
+            </Link>
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
